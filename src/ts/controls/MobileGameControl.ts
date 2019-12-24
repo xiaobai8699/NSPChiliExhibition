9fe3869db2f2c9d332581d2c70e5e83e5d07f7ef
LiHong <xmwlcjt@qq.com>
2019-12-24 15:31:38 +0800

    实现移动端行走

--- src/ts/controls/MobileGameControl.ts
+++ src/ts/controls/MobileGameControl.ts
@@ -1,5 +1,14 @@
+//实现参考:
+//https://zhuanlan.zhihu.com/p/40881782
+//https://developer.mozilla.org/zh-CN/docs/Web/API/Touch_events
+//https://developer.mozilla.org/zh-CN/docs/Web/API/Element/getBoundingClientRect
+//https://developer.mozilla.org/zh-CN/docs/Web/API/CSS_Object_Model/Determining_the_dimensions_of_elements
+//https://docs.microsoft.com/en-us/previous-versions//hh781509(v=vs.85)
+//https://www.quirksmode.org/mobile/viewports2.html
+
 import * as THREE from 'three';
-import {IPlayerContol} from './IPlayerControl';
+import { IPlayerContol } from './IPlayerControl';
+
 
 class MobileGameControl implements IPlayerContol {
 
@@ -7,49 +16,100 @@ class MobileGameControl implements IPlayerContol {
 
     domElement: HTMLElement;
 
+    sw: HTMLElement = document.querySelector("#steering-wheel");
+    swd: HTMLElement = document.querySelector("#steering-wheel-dot");
+
+    swRect: DOMRect = this.sw.getBoundingClientRect();
+    swdRect: DOMRect = this.swd.getBoundingClientRect();
+
+    swdHalfWidth: number = this.swdRect.width / 2;
+    swdHalfHeight: number = this.swdRect.height / 2;
+
+    miniLeft: number = 0;
+    maxLeft: number = this.swRect.width - this.swdRect.width;
+    miniTop: number = 0;
+    maxTop: number = this.swRect.height - this.swdRect.height;
+
+    originOffsetTop: number = this.swd.offsetTop;
+    originOffsetLeft: number = this.swd.offsetLeft;
+
     constructor(object: THREE.Camera, domElement?: HTMLElement) {
 
-        //如何动态改变HTMLElement的css属性:
-        //https://developer.mozilla.org/zh-CN/docs/Web/API/Element/className
-        // https://developer.mozilla.org/zh-CN/docs/Web/API/HTMLElement/style
-        // https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Properties_Reference
-        // https://developer.mozilla.org/en-US/docs/Web/API/CSS_Object_Model/Determining_the_dimensions_of_elements
+        this.domElement = domElement;
+        this.object = object;
+
+        // https://github.com/lh2lyc/ForceLandscape
         this.domElement.classList.add("canvas-rotation");
         this.domElement.style.width = `${window.innerHeight}`;
         this.domElement.style.height = `${window.innerWidth}`;
 
-        this.domElement.addEventListener('touchstart', this.onTouchStart, false);
-        this.domElement.addEventListener('touchmove', this.onTouchMove, false);
+        this.swd.addEventListener('touchstart', this.onTouchStart, false);
+        this.swd.addEventListener('touchmove', this.onTouchMove, false);
+        this.swd.addEventListener('touchend', this.onTouchEnd, false);
     }
 
-    lastX: number;
-    lastY: number;
+    
+    lastMoveX: number = 0;
+    lastMoveY: number = 0;
 
-    xDelta: number = 0;
-    yDelta: number = 0;
+    moveXDelta: number = 0;
+    moveYDelta: number = 0;
 
-    hSpeed: number = 0.1;
-    vSpeed: number = 0.1;
-
+    isMoving: boolean = false;
 
     onTouchStart = (e: TouchEvent) => {
-        var evt = e.changedTouches[0];
-        this.lastX = evt.pageX;
-        this.lastY = evt.pageY;
+        e.preventDefault();
+        e.stopPropagation();
+
+        this.lastMoveX = 0;
+        this.lastMoveY = 0;
+        this.moveXDelta = 0;
+        this.moveYDelta = 0;
+
+        this.isMoving = true;
     }
 
     onTouchMove = (e: TouchEvent) => {
-        var evt = e.changedTouches[0];
-        this.xDelta += (evt.pageX - this.lastX) * this.hSpeed;
-        this.yDelta += (evt.pageY - this.lastY) * this.vSpeed;
-        this.lastX = evt.pageX;
-        this.lastY = evt.pageY;       
+        e.preventDefault();
+        e.stopPropagation();
+
+        const touch = e.changedTouches.item(0);
+
+        const left = touch.clientX - this.swdHalfWidth - this.sw.offsetLeft;
+        if (left > this.miniLeft && left < this.maxLeft) {
+            this.swd.style.left = left.toString();
+
+            const x = (left + this.swdHalfWidth) - (this.swRect.width / 2)
+            this.moveXDelta = this.lastMoveX - x;
+            this.lastMoveX = x;
+        }
+
+        const top = touch.clientY - this.swdHalfHeight - this.sw.offsetTop;
+        if (top > this.miniTop && top < this.maxTop) {
+            this.swd.style.top = top.toString();
+
+            const y = (top + this.swdHalfHeight) - (this.swRect.height / 2);
+            this.moveYDelta = this.lastMoveY - y;
+            this.lastMoveY = y;
+        }
     }
 
-    update = (delta: number) => {
-        this.object.rotation.y = THREE.Math.degToRad(this.xDelta);
-        // this.object.rotation.x = THREE.Math.degToRad(this.yDelta);
+    onTouchEnd = (e: TouchEvent) => {
+        this.swd.style.left = this.originOffsetLeft.toString();
+        this.swd.style.top = this.originOffsetTop.toString();
 
+        this.isMoving = false;
+    }
+
+    moveSpeed: number = 9000;
+
+    update = (delta: number) => {
+        if(this.isMoving) {
+            const z = this.moveXDelta * delta * this.moveSpeed;
+            const x = this.moveYDelta * delta * this.moveSpeed;
+            this.object.translateZ(z);
+            this.object.translateX(-x);
+        }
     }
 
     dispose = () => {
@@ -59,4 +119,4 @@ class MobileGameControl implements IPlayerContol {
 }
 
 
-export {MobileGameControl};
+export { MobileGameControl };
