990645cb2b0f789016a42835b0b756d89e3941f1
LiHong <xmwlcjt@qq.com>
2019-12-25 15:27:00 +0800

    remove gltf mode and textures

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,14 +1,16 @@
 /*
  * @Author: Li Hong (lh.work@qq.com) 
  * @Date: 2019-12-25 08:44:37 
- * @Last Modified by:   Li Hong (lh.work@qq.com) 
- * @Last Modified time: 2019-12-25 08:44:37 
+ * @Last Modified by: Li Hong (lh.work@qq.com)
+ * @Last Modified time: 2019-12-25 15:12:24
  */
 
 
 import * as THREE from 'three';
 import { GLTFLoader, GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
 import { PlayerContol } from './controls/PlayerControl';
+import { PickupManager } from './pickup/PickupManager';
+
 import { Utils } from './utils/Utils';
 // import * as dat from 'dat.gui';
 import * as Stats from 'stats.js';
@@ -27,6 +29,8 @@ class App {
 
     pControl: PlayerContol;
 
+    pickupManager: PickupManager;
+
     // gui: dat.GUI;
 
     stats: Stats;
@@ -34,18 +38,23 @@ class App {
     public constructor() {
 
         let canvas: HTMLCanvasElement = document.querySelector("#canvas");
+
         let w = Utils.isMobile() ? window.innerHeight : window.innerWidth;
-        let h = Utils.isMobile() ? window.innerWidth  : window.innerHeight; 
+        let h = Utils.isMobile() ? window.innerWidth : window.innerHeight;
 
         let opt = null;
-        if(Utils.isMobile()){
-            opt = { canvas, antialias: false,logarithmicDepthBuffer:true };
-        }else {
-           opt = { canvas, antialias: true,logarithmicDepthBuffer:true }
+        if (Utils.isMobile()) {
+
+            opt = { canvas, antialias: false, logarithmicDepthBuffer: true };
+
+        } else {
+
+            opt = { canvas, antialias: true, logarithmicDepthBuffer: true }
+            
         }
 
         this.renderer = new THREE.WebGLRenderer(opt);
-        this.renderer.setSize(w,h);
+        this.renderer.setSize(w, h);
         this.renderer.setPixelRatio(Utils.devicePixelRatio());
 
         this.scene = new THREE.Scene();
@@ -60,6 +69,8 @@ class App {
 
         this.pControl = new PlayerContol(this.camera, this.renderer.domElement);
 
+        this.pickupManager = new PickupManager(this.camera, this.scene, canvas);
+
         this.clock = new THREE.Clock();
 
         this.stats = new Stats();
@@ -71,15 +82,18 @@ class App {
     }
 
     run = (gltf: GLTF) => {
+
         this.addLights();
         this.scene.add(gltf.scene);
         this.renderLoop();
         this.repositionCamera();
         this.playVideo();
         this.debug();
+
     }
 
     renderLoop = () => {
+        
         this.stats.begin();
         {
             this.renderer.render(this.scene, this.camera);
@@ -89,9 +103,11 @@ class App {
         this.stats.end();
 
         requestAnimationFrame(this.renderLoop);
+
     }
 
     addLights = () => {
+
         {
             const skyColor = 0xffffff;
             const groundColor = 0xffffff;
@@ -107,8 +123,8 @@ class App {
             const intensity = 2;
             const light = new THREE.DirectionalLight(color, intensity);
             light.position.set(0, 10000, 0);
-            light.lookAt(0, 0, 0);
-            this.scene.add(light);
+            // this.scene.add(light);
+            this.camera.add(light);
         }
 
         const m = new THREE.MeshStandardMaterial({ color: 0xff0000 });
@@ -117,15 +133,19 @@ class App {
         const scaler = 1000;
         mesh.position.set(0, 1, 0);
         this.scene.add(mesh);
+
     }
 
     onWindowResize = () => {
+
         this.renderer.setSize(window.innerWidth, window.innerHeight);
         this.camera.aspect = window.innerWidth / window.innerHeight;
         this.camera.updateProjectionMatrix();
+
     }
 
     repositionCamera = () => {
+
         const box = new THREE.Box3().setFromObject(this.scene);
         const length = box.getSize(new THREE.Vector3()).length();
         const center = box.getCenter(new THREE.Vector3());
@@ -135,11 +155,12 @@ class App {
         const pos = center.clone().setY(y).setZ(10000);
         const cen = center.clone().setY(y).setZ(center.z - 4000);
 
-        this.camera.near =  0.1;
+        this.camera.near = 0.1;
         this.camera.far = length * 100;
         this.camera.position.copy(pos);
         this.camera.lookAt(cen);
         this.camera.updateProjectionMatrix();
+
     }
 
     bigTV: THREE.Object3D = null;
@@ -149,11 +170,13 @@ class App {
     speed: number = 0.003;
 
     animateAd = () => {
+
         this.bigTV = this.bigTV || this.scene.getObjectByName("BIG_AD");
         this.smallTV = this.smallTV || this.scene.getObjectByName("SMALL_AD");
 
         this.bigTV.rotation.y += this.speed;
         this.smallTV.rotation.y -= this.speed;
+
     }
 
 
@@ -161,6 +184,7 @@ class App {
     // https://www.aerserv.com/blog/why-does-video-autoplay-on-mobile-devices-not-work/
     // https://www.google.com/search?sxsrf=ACYBGNSWYbUUOlnNjrq-USPBftDSpPX1Kw%3A1576825687684&source=hp&ei=V3P8XYydJ5iSr7wP-tWggAE&q=can+video+autoplay+on+mobile&oq=video+can%27t+autoplay&gs_l=psy-ab.1.6.0i13i30j0i13i5i30l2j0i8i13i30l5.2832.15948..24624...4.0..0.195.3646.0j22......0....1..gws-wiz.....10..35i362i39j0j0i10j0i13j0i10i30j0i19j0i12i30i19j0i12i10i30i19j33i160.1-ba9bWx3VU
     playVideo = () => {
+        
         const video: HTMLVideoElement = document.querySelector("#video");
         video.volume = 1.0;
         video.muted = false;
@@ -183,7 +207,7 @@ class App {
 
     }
 
-    debug = ()=>{
+    debug = () => {
         // this.gui = new dat.GUI();
 
 
@@ -194,11 +218,11 @@ class App {
 
         //  const data = {
         //     playerControl:function () {
-                 
+
         //     },
 
         //     orbitControls: function () {
-                
+
         //     }
         //  }
         // const cameraFloder = this.gui.addFolder('控制器');
@@ -208,39 +232,46 @@ class App {
 
 }
 
-const progress: HTMLElement = document.querySelector("#progres-fill");
-const progressText: HTMLElement = document.querySelector("#progress-text");
-const ui: HTMLElement = document.querySelector("#ui");
-const steeringWheel: HTMLElement = document.querySelector("#steering-wheel");
+(function main(){
 
-new GLTFLoader().load(
-    './asset/model/NSP_NG.gltf',
+    const ui: HTMLElement = document.querySelector("#ui");
+    const progress: HTMLElement = document.querySelector("#progres-fill");
+    const progressText: HTMLElement = document.querySelector("#progress-text");
+    const steeringWheel: HTMLElement = document.querySelector("#steering-wheel");
 
-    glft => {
-        try {
-            const app = new App();
-            app.run(glft);
-
-            ui.style.display = "none";
-            if(Utils.isMobile()){
-                 steeringWheel.style.visibility = "visible";
+    new GLTFLoader().load(
+        './asset/model/model.glb',
+    
+        glft => {
+            try {
+                const app = new App();
+                app.run(glft);
+    
+                ui.style.display = "none";
+                if (Utils.isMobile()) {
+                    steeringWheel.style.visibility = "visible";
+                }
             }
+            catch (e) {
+                alert(`应用异常(${e})`);
+            }
+        },
+    
+        xhr => {
+            let p = (xhr.loaded / xhr.total) * 100;
+            progress.style.width = `${p}%`;
+            progressText.innerText = `${p.toFixed(2)}%`;
+            if (p >= 100) {
+                progressText.innerText = "正在进入展馆，请稍候..";
+            }
+        },
+    
+        err => {
+            alert(`加载资源失败(${err})`);
         }
-        catch (e) {
-            alert(`应用异常(${e})`);
-        }
-    },
+    ); 
 
-    xhr => {
-        let p = (xhr.loaded / xhr.total) * 100;
-        progress.style.width = `${p}%`;
-        progressText.innerText = `正在加载资源 ${p}%`;
-        if(p >= 100){
-            progressText.innerText = "正在进入展馆，请稍候..";
-        }
-    },
+})();
 
-    err => {
-        alert(`加载资源失败(${err})`);
-    }
-); 
+
+
