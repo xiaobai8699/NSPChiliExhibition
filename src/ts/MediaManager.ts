2bcb1fda6e10501847c7733b8ce1fd6849c4fc93
LiHong <xmwlcjt@qq.com>
2019-12-26 16:02:28 +0800

    Create MediaManager.ts

---
+++ src/ts/MediaManager.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: Li Hong (lh.work@qq.com) 
+ * @Date: 2019-12-26 13:05:05 
+ * @Last Modified by: Li Hong (lh.work@qq.com)
+ * @Last Modified time: 2019-12-26 13:05:57
+ */
+
+import * as THREE from 'three';
+
+export  class MediaManager {
+
+}
