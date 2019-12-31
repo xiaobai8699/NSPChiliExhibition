06e1ba222cf3f150d44adc68e19d1094f3e683ef
LiHong <xmwlcjt@qq.com>
2019-12-31 11:46:56 +0800

    Update Const.ts

--- src/ts/Const.ts
+++ src/ts/Const.ts
@@ -2,18 +2,22 @@
  * @Author: Li Hong (lh.work@qq.com) 
  * @Date: 2019-12-30 17:50:19 
  * @Last Modified by: Li Hong (lh.work@qq.com)
- * @Last Modified time: 2019-12-30 17:58:33
+ * @Last Modified time: 2019-12-31 09:49:34
  */
 
 export class Const {
 
-    static nspLogName: string = "logo_D";
+    static readonly nspLogName: string = "logo_D";
 
-    static audioUrl: string = "";
+    static readonly audioUrl: string = "";
 
-    static videoUrl: string = "";
+    static readonly videoUrl: string = "";
 
-    static audienceUrl: string = "";
 
+    static visitorUrl(path: string): string {
+
+        return "./asset/image/visitor/"+ path + ".png";
+        
+    }
     
 }
