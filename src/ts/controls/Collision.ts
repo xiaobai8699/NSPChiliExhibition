7e672e37e7613513c729e97ebfef7f8c4584fcf0
LiHong <xmwlcjt@qq.com>
2020-01-03 15:43:48 +0800

    Update Collision.ts

--- src/ts/controls/Collision.ts
+++ src/ts/controls/Collision.ts
@@ -2,7 +2,7 @@
  * @Author: Li Hong (lh.work@qq.com) 
  * @Date: 2020-01-03 13:52:44 
  * @Last Modified by: Li Hong (lh.work@qq.com)
- * @Last Modified time: 2020-01-03 14:08:55
+ * @Last Modified time: 2020-01-03 15:43:33
  */
 
 //参考: https://docs.microsoft.com/zh-cn/windows/uwp/get-started/get-started-tutorial-game-js3d
@@ -16,14 +16,45 @@ import { Raycaster, Vector3 } from 'three';
 export class Collision {
 
 
-    static detect(): boolean {
+    static detect(moveForward=false,moveBackward=false,moveLeft=false,moveRight=false): boolean {
 
-        let origin = new THREE.Vector3();
-        World.x().camera.getWorldPosition(origin);
+        //不移动就不检测
+        if((moveBackward || moveBackward || moveLeft || moveRight) == false) {
+            return;
+        }
+
+        let rotationMatrix: THREE.Matrix4;
+
+        if(moveBackward) {
+
+            rotationMatrix = new THREE.Matrix4();
+            rotationMatrix.makeRotationY(THREE.Math.degToRad(180));
+        }
+
+        else if(moveLeft) {
+
+            rotationMatrix = new THREE.Matrix4();
+            rotationMatrix.makeRotationY(THREE.Math.degToRad(90));
+        }
+
+        else if(moveRight) {
+
+            rotationMatrix = new THREE.Matrix4();
+            rotationMatrix.makeRotationY(THREE.Math.degToRad(270));
+        }
 
         let direction = new THREE.Vector3();
         World.x().camera.getWorldDirection(direction);
+        
+        if(rotationMatrix != undefined) {
+            //将光线投射方向与移动方向保持一直
+            direction.applyMatrix4(rotationMatrix);
+        }
         direction = direction.normalize();
+
+        let origin = new THREE.Vector3();
+        World.x().camera.getWorldPosition(origin);
+        origin.y = 0.2;
 
         const rayCaster = new Raycaster(origin,direction);
 
@@ -34,7 +65,7 @@ export class Collision {
             for (let i = 0; i < intersectedObjects.length; i++ ){
 
                 if(intersectedObjects[i].distance < 2) {
-                    console.log(`[Collision]: ${intersectedObjects[i].object.name}`);
+
                     return true;
                 }
 
@@ -44,7 +75,6 @@ export class Collision {
 
         }else {
 
-            console.log("[Collision]: not dectect objects");
             return false;
         }
     }
