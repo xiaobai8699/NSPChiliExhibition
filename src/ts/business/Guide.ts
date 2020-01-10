7806aa1758048091319e5debce5832a63b5fa92b
LiHong <xmwlcjt@qq.com>
2020-01-10 18:41:26 +0800

    Create Guide.ts

---
+++ src/ts/business/Guide.ts
@@ -0,0 +1,17 @@
+/*
+ * @Author: Li Hong (lh.work@qq.com) 
+ * @Date: 2020-01-10 18:26:42 
+ * @Last Modified by: Li Hong (lh.work@qq.com)
+ * @Last Modified time: 2020-01-10 18:29:56
+ */
+
+
+ export class Guide {
+
+    static show(){
+
+        
+
+    }
+  
+ }
