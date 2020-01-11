2ad4f0d7e5f9a4e70c527b9ffa4c7d7427807b36
LiHong <xmwlcjt@qq.com>
2020-01-11 15:55:52 +0800

    Update Video.ts

--- src/ts/common/core/Video.ts
+++ src/ts/common/core/Video.ts
@@ -2,7 +2,7 @@
  * @Author: Li Hong (lh.work@qq.com) 
  * @Date: 2019-12-26 13:05:05 
  * @Last Modified by: Li Hong (lh.work@qq.com)
- * @Last Modified time: 2020-01-11 15:46:34
+ * @Last Modified time: 2020-01-11 15:55:05
  */
 
 // 为什么在移动设备上无法自动播放视频:
@@ -113,9 +113,15 @@ export class Video {
         event.preventDefault();
         event.stopPropagation();
 
-        //Press p key to play
-        if (event.keyCode == 80) {
-            this.play();
+        console.log(event.keyCode);
+        switch(event.keyCode){
+            case 80: //press p
+                this.play();
+                break;
+
+            case 79: //press o
+                 this.video.pause();
+                 break;
         }
     }
 }
