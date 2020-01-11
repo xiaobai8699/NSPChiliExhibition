1ffe9782968fab5781478a12aa12296a0a3b7001
LiHong <xmwlcjt@qq.com>
2020-01-11 15:49:28 +0800

    Update Chili.ts

--- src/ts/business/Chili.ts
+++ src/ts/business/Chili.ts
@@ -2,7 +2,7 @@
  * @Author: Li Hong (lh.work@qq.com) 
  * @Date: 2020-01-10 09:24:19 
  * @Last Modified by: Li Hong (lh.work@qq.com)
- * @Last Modified time: 2020-01-10 21:13:26
+ * @Last Modified time: 2020-01-11 14:04:12
  */
 
 import * as THREE from 'three';
@@ -53,8 +53,9 @@ export class Chili {
                 if (o.material.name == "pingshenA") {
 
                     o.material.transparent = true;
-                    o.material.opacity = 0.6;
-                    o.material.side = THREE.DoubleSide;
+                     o.material.opacity = 0.6;
+                    o.material.side = THREE.FrontSide;
+                    o.material.stencilWrite = true;
                 }
             });
         });
