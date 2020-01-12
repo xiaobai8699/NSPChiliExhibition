eb5858f7f58a2fb2e091653a07db5405f84bdbb8
LiHong <xmwlcjt@qq.com>
2020-01-12 10:21:24 +0800

    只在安卓H5网页播放背景音乐

--- src/ts/common/core/Audio.ts
+++ src/ts/common/core/Audio.ts
@@ -9,6 +9,7 @@ import * as THREE from 'three';
 import { Vector3, AudioLoader } from 'three';
 import {World} from '../World';
 import { Const } from '../../business/Const';
+import { Utils } from '../Utils';
 
 let audioInstance: Audio;
 
@@ -21,29 +22,39 @@ export class Audio {
 
     }
 
-    static play(): void {
+    static playBgm(){
+        //只在安卓机上播放背景音乐(但不在安卓小程序内播放)
+        //因为：小程序和iOS均可播放视频，但安卓的微信H5页面不能播放视频，所以播放背景音乐替代
+        if(Utils.isAndroid()){
+            if(!Utils.inWxMiniApp()){
 
+                const listener = new THREE.AudioListener();
+                World.x().camera.add(listener);
         
-        const listener = new THREE.AudioListener();
-        World.x().camera.add(listener);
-
-        const sound = new THREE.PositionalAudio(listener);
-
-        const audioLoader = new THREE.AudioLoader();
+                const sound = new THREE.PositionalAudio(listener);
         
-        audioLoader.load(Const.audioUrl, (buffer: any) =>{
-            
-            sound.setBuffer(buffer);
-            sound.setRefDistance(10); // 距离声源10开始衰减
-            sound.setRolloffFactor(5);
-            sound.setLoop(true);
-            sound.play();
+                const audioLoader = new THREE.AudioLoader();
+                
+                audioLoader.load(Const.audioUrl, (buffer: any) =>{
+                    
+                    sound.setBuffer(buffer);
+                    sound.setRefDistance(20); // 距离声源10开始衰减
+                    sound.setRolloffFactor(5);
+                    sound.setLoop(true);
+                    sound.play();
+        
+                });
+        
+                const logMesh = World.x().scene.getObjectByName(Const.nspLogName);
+                logMesh.add(sound);
+            }
+        }
+    }
 
-        });
+    static play() {
 
-        const logMesh = World.x().scene.getObjectByName(Const.nspLogName);
-        logMesh.add(sound);
-
+        this.playBgm();
+        
     }
 
 }
